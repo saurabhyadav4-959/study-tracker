445411fb78cfbcff5b642cee81060040ec8bed93
saurabhyadav4-959 <saurabhy4959@gmail.com>
2026-05-19 17:29:20 +0530

    chore: Update Google Client ID credentials

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './index.css'
 import { AppProvider } from './context/AppContext'
 import { GoogleOAuthProvider } from '@react-oauth/google'
 
-const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || '238388973814-hh2h6gae19fdq0ku9g9capvvg0664tba.apps.googleusercontent.com';
+const GOOGLE_CLIENT_ID = import.meta.env.VITE_GOOGLE_CLIENT_ID || '238388973814-72776kvvco1uqts1fhof9q68p9a13l73.apps.googleusercontent.com';
 console.log("Loaded Google Client ID:", GOOGLE_CLIENT_ID ? "YES (Found)" : "NO (Empty)");
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
